730d37e5edf568f0e324c6e987bf089c3a4c3f3a
sauprankul <sauprankul@users.noreply.github.com>
2020-09-24 14:57:13 -0500

    Create test.py

---
+++ test.py
@@ -0,0 +1,14 @@
+"""
+HW3 has not been published yet, so this tester is still empty.
+Once one of the admins understands the assignments, we will publish an unverified draft
+of the test.
+Once one of the admins solves HW3 or receives satisfactory output from someone else, a ref.txt
+will be uploaded
+
+TL;DR: Hang tight
+"""
+
+__author__ = "cs540-testers"
+__credits__ = ["Saurabh Kulkarni", "Alex Moon", "Stephen Jasina", "haclark"]
+
+print("HW3 Tester")
